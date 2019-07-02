13e980606ed6c3572ad311ddcd7bc25630bd3c4a
Christopher Williams <cw@chriswillia.ms>
2019-07-01 22:15:46 -0500

    Provides authorization of requests and implements checking query parameters and setting useful defaults if they are not set in the request.

--- server.js
+++ server.js
@@ -9,13 +9,44 @@ const MOVIE_DATA = require('./movies-data-small.json');
 const app = express();
 
 app.use(morgan('common'));
+app.use(function validateHeaderAuthorization(req, res, next) {
+    let apiKey = process.env.API_KEY;
+    let authKey = req.get('Authorization');
+    if(!authKey || authKey.split(' ')[1] !== apiKey) {
+        return res.status(401).json({'error': 'request could not be authorized'})
+    }
+    next();
+});
 app.use(cors());
 app.use(helmet());
 
-app.get('/', handleMovieRequest);
+app.get('/movie', handleMovieRequest);
 
 function handleMovieRequest(req, res) {
-    res.json(MOVIE_DATA);
+    // accept genre, country, or avg_vote search query params
+    // genre: string - case insensitive search
+    // country: string - case insensitive search
+    // avg_vote: Number(string) -  >= the supplied
+debugger;
+    let {genre, country, avg_vote} = req.query;
+
+    // some validation and setting defualts
+    if(!genre) {
+        genre = '';
+    }
+
+    if(!country) {
+        country = '';
+    }
+
+    if(avg_vote) {
+        avg_vote = parseFloat(avg_vote);
+        if(Number.isNaN(avg_vote)) {
+            return res.status(400).json({'error': 'avg_vote must be supplied as a number'})
+        }
+    }
+
+    res.send('awesome');
 }
 
 const PORT = 8000;
